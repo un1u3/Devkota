a5fa4653eb01a88aee93e891e4b3f09a9c20fc21
un1u3 <python.github.devs@gmail.com>
2026-02-03 14:40:07 +0545

    devkota: working on constructor and finding good numbers for hyperparameters

---
+++ main/devkota.py
@@ -0,0 +1,25 @@
+import torch 
+import torch.nn as nn 
+import torch.nn fucntional as F 
+from src.core.positionalencoder import PositionalEncoding
+from src.core.multi_head_attention import MultiHeadAttention, create_casual_mask, create_padding_mask
+from transformer import TransformerBlock
+
+
+class Devkota(nn.Module):
+    # architecture
+    # 1.TOken embedding 
+    # 2.positional encoding 
+    # 3. 12 transformer blocks 
+    # 4.Finallayer norm 
+    # lang model head (projectt to vocab)
+
+
+    def __init__(self, vocab_size, d_model,num_layers, num_heads,d_ff, max_seq_len, dropout, pad_idx):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.d_model = d_model
+        self.num_layers = num_layers
+        self.max_seq_len = max_seq_len
+        self.pad_idx = pad_idx
+        
